70cef1b32955b7b7920f029e1c5f78b2ed1c20d3
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-04-15 20:21:11 -0700

    update

--- energy_production.py
+++ energy_production.py
@@ -1,15 +1,51 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
+import random
+import matplotlib.animation as animation
+import pandas as pd
+import datetime
+import seaborn as sns
 
-energy_path = "./data/france_production.csv"
+df = pd.read_csv("./data/france_weather_energy_with_date.csv")
 
-energy_df = pd.read_csv(energy_path)
+df["dt_iso"] = pd.to_datetime(df["dt_iso"])
 
-solar = energy_df[energy_df["Date and Hour"].str.contains("2021") & energy_df["Source"].str.contains("Solar")]
-wind = energy_df[energy_df["Date and Hour"].str.contains("2021") & energy_df["Source"].str.contains("Wind")]
-fig, ax = plt.subplots(figsize=(16, 8))
-ax.scatter(solar["Date and Hour"], solar["Production"], c="yellow", label="Solar", marker="o", linewidths=0.5)
-ax.scatter(wind["Date and Hour"], wind["Production"], c="blue", label="Wind", marker="x", linewidths=0.5)
+number_of_days = 7
+start_date = "2022-01-13"
+end_date = datetime.datetime.strptime(start_date, "%Y-%m-%d") + datetime.timedelta(days=number_of_days)
+end_date = end_date.strftime("%Y-%m-%d")
+df_plot = df[df["dt_iso"] >= start_date]
+df_plot = df_plot[df_plot["dt_iso"] <= end_date]
+plt.figure(figsize=(14, 7))
+plt.plot(df_plot["dt_iso"], df_plot["production_wind"], label="Wind")
+plt.plot(df_plot["dt_iso"], df_plot["production_solar"], label="Solar")
+plt.xlabel("Date")
+plt.ylabel("Production (MW)")
 plt.legend()
-plt.savefig("energy_production.png")
+plt.savefig("energy_production_2.png")
+
+
+fig, ax = plt.subplots(figsize=(14, 10))
+line1, = ax.plot([], [], label="Wind")
+line2, = ax.plot([], [], label="Solar")
+ax.legend()
+ax.set_ylim(0, max(df["production_wind"].max(), df["production_solar"].max()))
+ax.set_xlabel("Date")
+ax.set_ylabel("Production (MW)")
+def animate(i):
+    start_date = df["dt_iso"].min() + datetime.timedelta(days=i)
+    end_date = start_date + datetime.timedelta(days=number_of_days)
+    df_plot = df[(df["dt_iso"] >= start_date) & (df["dt_iso"] <= end_date)]
+    line1.set_data(df_plot["dt_iso"], df_plot["production_wind"])
+    line2.set_data(df_plot["dt_iso"], df_plot["production_solar"])
+    ax.set_xlim(start_date, end_date)
+    return line1, line2
+
+ani = animation.FuncAnimation(fig, 
+                              animate, 
+                              frames=int((df["dt_iso"].max() - df["dt_iso"].min()).days - number_of_days + 1),
+                              interval=75,
+                              blit=True)
+ani.save("energy_production.mp4")
